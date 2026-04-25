565ee683ea266692cf6697479dfac6b365f87744
HaydenQ-creator <hquach@ftstudent.org>
2026-04-24 21:09:46 -0400

    no debug

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777074758|1019909772';
+const CACHE_VERSION = '1777079371|136117996';
 /** @type {string} */
 const CACHE_PREFIX = 'Survival the Lav-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
